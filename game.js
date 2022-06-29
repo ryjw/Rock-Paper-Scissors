432a39caefc0c29736bc60920a16db6f9e4ab226
Ryan Wilson <ryanjoelwilson@gmail.com>
2022-06-29 18:23:43 +0100

    added computerPlay function

--- game.js
+++ game.js
@@ -1,3 +1,11 @@
 function computerPlay () {
-    
+    const computerGuess = Math.floor(Math.random() * 3)
+    switch (computerGuess) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
 }
