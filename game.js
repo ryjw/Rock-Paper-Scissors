90f33cbc93b1d10e06829905daff89fb0efd8d95
Ryan Wilson <ryanjoelwilson@gmail.com>
2022-06-29 21:51:21 +0100

    Finished the game

--- game.js
+++ game.js
@@ -16,19 +16,43 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return 'Draw!'
     }
-    else if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         return 'You win! Rock beats scissors.';
-    } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         return 'You win! Paper beats rock.';
-    } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
          return 'You win! Scissors beat paper.';
-    } else if (playerSelection === 'rock' && computerSelection === 'Paper') {
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         return 'You lose! Paper beats rock.';
-    } else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         return 'You lose! Scissors beat paper.';
-    } else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         return 'You lose! Rock beats scissors'
     } else {
         return 'Please choose between rock, paper, or scissors'
+    }
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerChoice = prompt("Rock, paper, or scissors?");
+        const computerChoice = computerPlay();
+        const result = playRound(playerChoice, computerChoice);
+        const message = `Round ${i + 1}/5: ${result}`;
+        if (result.startsWith('You win')) {
+            playerScore += 1;
+        } else if (result.startsWith('You lose')) {
+            computerScore += 1;
+        }
+        console.log(message);
+    }
+    if (playerScore > computerScore) {
+        console.log(`Congratulations! You won with ${playerScore} overall victories!`)
+    } else if (computerScore > playerScore) {
+        console.log(`Unlucky! The computer won with ${computerScore} victories...`)
+    } else {
+        console.log(`The games resulted in a draw overall!`)
     }
 }
