5f81d141c6d5d3131c3168e62759dbc5efcf4680
Ryan Wilson <ryanjoelwilson@gmail.com>
2022-06-29 18:44:11 +0100

    Wrote game logic in playRound function

--- game.js
+++ game.js
@@ -9,3 +9,24 @@ function computerPlay () {
             return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (toLowerCase(playerSelection) === toLowerCase(computerSelection)) {
+        return 'Draw!'
+    }
+    else if (toLowerCase(playerSelection) === 'rock' && computerSelection === 'Scissors') {
+        return 'You win! Rock beats scissors.';
+    } else if (toLowerCase(playerSelection) === 'paper' && computerSelection === 'Rock') {
+        return 'You win! Paper beats rock.';
+    } else if (toLowerCase(playerSelection) === 'scissors' && computerSelection === 'Paper') {
+         return 'You win! Scissors beat paper.';
+    } else if (toLowerCase(playerSelection) === 'rock' && computerSelection === 'Paper') {
+        return 'You lose! Paper beats rock.';
+    } else if (toLowerCase(playerSelection) === 'paper' && computerSelection === 'Scissors') {
+        return 'You lose! Scissors beat paper.';
+    } else if (toLowerCase(playerSelection) === 'scissors' && computerSelection === 'Rock') {
+        return 'You lose! Rock beats scissors'
+    } else {
+        return 'Please choose between rock, paper, or scissors'
+    }
+}
